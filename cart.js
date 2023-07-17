f7b85f5a1afd85087c7171c1cb014331f39bb7f0
LedinaShahi <104737671+LedinaShahi@users.noreply.github.com>
2023-07-17 14:22:14 +0200

    l.sh/searchBar/

--- cart.js
+++ cart.js
@@ -8,3 +8,39 @@ list.addEventListener('click', e => {
 });
 
 //search item
+
+
+
+var searchInput = document.getElementById("searchInput");
+
+
+
+searchInput.addEventListener("input", function() {
+    var searchValue = searchInput.value.toLowerCase(); 
+    
+    var tdElements = document.getElementsByClassName("sname");
+    console.log(tdElements)
+   
+
+    
+    for (var i = 0; i < tdElements.length; i++) {
+        var td = tdElements[i];
+
+        
+        var productText = td.textContent.toLowerCase();
+
+        
+        var listItem = td.parentElement;
+        console.log(productText.includes(searchValue))
+        
+        if (productText.includes(searchValue)) {
+            
+            listItem.style.display = "block";
+        } else {
+            
+            listItem.style.display = "none";
+        }
+    }
+});
+
+       
